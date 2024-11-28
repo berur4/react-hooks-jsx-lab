f987c793a9ed153cbe036b6a4c7cb53d52a442e7
berur4 <berurcaleb@gmail.com>
2024-11-28 23:14:09 +0300

    jsx lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
+// src/components/About.js
+
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js"; // Correct path to your user.js file
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I made this portfolio to showcase my skills and projects.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
